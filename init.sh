d0daade2810fe1876d86c5b0e45f87040eb4aa25
Sergey Sadovnikov <sergey.sadovnikov@gmail.com>
2025-05-04 04:34:23 +0300

    delete init.sh

--- init.sh
+++
@@ -1,6 +0,0 @@
-echo '{"warnings": {}, "banned": {}, "restricted_users": {"no_links": {}, "fully_restricted": {}, "no_forwards": {}}, "user_stats": {}}' > /app/data/moderation_data.json
-echo '{}' > /app/user_stats.json
-touch /app/moderation.log
-chmod 666 /app/*.json
-chmod 666 /app/*.log
-exec python bot.py
